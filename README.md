1197145dd4ea550e1a40936b7d7f02f9b34c3a24
David Gorski <gorski.dave@gmail.com>
2024-06-12 10:00:26 -0400

    - Add std::sync benchmark for queue - Add cache line padding for better performance.

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Strengthening my atomic operation knowledge with some hands-on coding.
     - Average time to read: 57 ns, total reads: 116,812,897.
     - Average time to write: 29 ns, total writes: 172,497,080.
 - [WaitFreeQueue](./src/waitfree_queue.rs): Inter-thread queue with lock and wait free reading, lock-free writing. Results on M1 Macbook Air:
-    - Average time to read: 41 ns, total reads: 145,907,643.
-    - Average time to write: 41 ns, total writes: 145,698,898.
+    - Average time to read: 22 ns, total reads: 203,991,606.
+    - Average time to write: 36 ns, total writes: 156,541,568.
 - [WaitFreeWorker](./src/waitfree_worker.rs): Single background thread worker that leverages wait-free queues to allow rapid scheduling of tasks and subsequent callbacks. Results on M1 Macbook Air:
     - Average posting time: 78 ns.
     - Average polling time: 31 ns.
--- src/waitfree_queue.rs
+++ src/waitfree_queue.rs
@@ -1,12 +1,17 @@
 use std::{array, sync::atomic::{AtomicUsize, Ordering}};
 
+#[repr(align(64))]
+struct CachePadded<T>{
+    value: T
+}
+
 /// A wait-free queue that can be used to send and receive values between threads.
 pub struct WaitFreeQueue<T, const SIZE: usize> {
-    read_sequence: AtomicUsize,
-    read_sequence_cached: usize,
-    write_sequence: AtomicUsize,
-    write_sequence_cached: usize,
-    buffer: [Option<T>; SIZE],
+    read_sequence: CachePadded<AtomicUsize>,
+    read_sequence_cached: CachePadded<usize>,
+    write_sequence: CachePadded<AtomicUsize>,
+    write_sequence_cached: CachePadded<usize>,
+    buffer: [CachePadded<Option<T>>; SIZE],
 }
 
 fn get_next_index(value: usize, max: usize) -> usize {
@@ -21,50 +26,50 @@ fn get_next_index(value: usize, max: usize) -> usize {
 impl<T, const SIZE: usize> WaitFreeQueue<T, SIZE> {
 
     fn get_read_index(&mut self, next_write_index: usize) -> usize {
-        if self.read_sequence_cached == next_write_index {
-            self.read_sequence_cached = self.read_sequence.load(Ordering::Acquire);
+        if self.read_sequence_cached.value == next_write_index {
+            self.read_sequence_cached.value = self.read_sequence.value.load(Ordering::Acquire);
         }
-        self.read_sequence_cached
+        self.read_sequence_cached.value
     }
 
     fn get_write_index(&mut self, next_read_index: usize) -> usize {
-        if self.write_sequence_cached == next_read_index {
-            self.write_sequence_cached = self.write_sequence.load(Ordering::Acquire);
+        if self.write_sequence_cached.value == next_read_index {
+            self.write_sequence_cached.value = self.write_sequence.value.load(Ordering::Acquire);
         }
-        self.write_sequence_cached
+        self.write_sequence_cached.value
     }
 
     /// Creates a new `WaitFreeQueue`.
     pub fn new() -> Self {
         WaitFreeQueue {
-            buffer: array::from_fn(|_| None),
-            read_sequence: AtomicUsize::new(0),
-            read_sequence_cached: 0,
-            write_sequence: AtomicUsize::new(0),
-            write_sequence_cached: 0,
+            buffer: array::from_fn(|_| CachePadded{ value: None }),
+            read_sequence: CachePadded{ value: AtomicUsize::new(0) },
+            read_sequence_cached: CachePadded{ value: 0 },
+            write_sequence: CachePadded{ value: AtomicUsize::new(0) },
+            write_sequence_cached: CachePadded{ value: 0 },
         }
     }
 
     /// Tries to write a value to the queue.
     pub fn try_write(&mut self, value: T) -> bool {
-        let write_index = self.write_sequence.load(Ordering::Relaxed);
+        let write_index = self.write_sequence.value.load(Ordering::Relaxed);
         let next_write_index = get_next_index(write_index, SIZE);
         if next_write_index == self.get_read_index(next_write_index) {
             return false;
         }
-        self.buffer[write_index] = Some(value);
-        self.write_sequence.store(next_write_index, Ordering::Release);
+        self.buffer[write_index].value = Some(value);
+        self.write_sequence.value.store(next_write_index, Ordering::Release);
         true
     }
 
     /// Tries to read a value from the queue.
     pub fn try_read(&mut self) -> Option<&mut T> {
-        let read_index = self.read_sequence.load(Ordering::Relaxed);
+        let read_index = self.read_sequence.value.load(Ordering::Relaxed);
         if read_index == self.get_write_index(read_index) {
             return None;
         }
-        let result = self.buffer[read_index].as_mut().unwrap();
-        self.read_sequence.store(get_next_index(read_index, SIZE), Ordering::Release);
+        let result = self.buffer[read_index].value.as_mut().unwrap();
+        self.read_sequence.value.store(get_next_index(read_index, SIZE), Ordering::Release);
         Some(result)
     }
 }
@@ -110,15 +115,63 @@ mod tests {
         let running = AtomicBool::new(true);
         let mut queue = Box::new(WaitFreeQueue::<i64, 1024>::new());
 
-        let reader_running = unsafe{ AtomicBool::from_ptr(running.as_ptr()) };
+        let reader_running = unsafe{ CachePadded{ value: AtomicBool::from_ptr(running.as_ptr()) } };
         let mut reader_sneaker = ThreadSneaker::new(&mut queue);
         let reader_thread = std::thread::spawn(move || {
             let mut total_nanoseconds_reading = 0;
             let mut total_reads = 0;
             let queue = reader_sneaker.get_item();
-            while reader_running.load(Ordering::Relaxed) {
+            while reader_running.value.load(Ordering::Relaxed) {
                 let start = std::time::Instant::now();
                 let _result = queue.try_read();
+                if _result.is_some() {
+                }
+                let duration = start.elapsed();
+                total_nanoseconds_reading += duration.as_nanos();
+                total_reads += 1;
+            }
+            (total_nanoseconds_reading, total_reads)
+        });
+
+        let writer_running = unsafe{ CachePadded{ value: AtomicBool::from_ptr(running.as_ptr()) } };
+        let mut writer_sneaker = ThreadSneaker::new(&mut queue);
+        let writer_thread = std::thread::spawn(move || {
+            let mut total_nanoseconds_writing = 0;
+            let mut total_writes = 0;
+            let queue = writer_sneaker.get_item();
+            while writer_running.value.load(Ordering::Relaxed) {
+                let start = std::time::Instant::now();
+                let _result = queue.try_write(112341234);
+                let duration = start.elapsed();
+                total_nanoseconds_writing += duration.as_nanos();
+                total_writes += 1;
+            }
+            (total_nanoseconds_writing, total_writes)
+        });
+
+        std::thread::sleep(std::time::Duration::from_secs(10));
+        running.store(false, Ordering::Relaxed);
+
+        let (total_nanoseconds_reading, total_reads) = reader_thread.join().unwrap();
+        let (total_nanoseconds_writing, total_writes) = writer_thread.join().unwrap();
+
+        // Print the results
+        println!("Average time to read: {} ns, total reads: {}", total_nanoseconds_reading / total_reads, total_reads);
+        println!("Average time to write: {} ns, total writes: {}", total_nanoseconds_writing / total_writes, total_writes);
+    }
+
+    #[test]
+    fn benchmark_stdsyncchannel() {
+        let running = AtomicBool::new(true);
+        let (tx, rx) = std::sync::mpsc::channel();
+
+        let reader_running = unsafe{ AtomicBool::from_ptr(running.as_ptr()) };
+        let reader_thread = std::thread::spawn(move || {
+            let mut total_nanoseconds_reading = 0;
+            let mut total_reads = 0;
+            while reader_running.load(Ordering::Relaxed) {
+                let start = std::time::Instant::now();
+                let _result = rx.try_recv();
                 let duration = start.elapsed();
                 total_nanoseconds_reading += duration.as_nanos();
                 total_reads += 1;
@@ -127,14 +180,12 @@ mod tests {
         });
 
         let writer_running = unsafe{ AtomicBool::from_ptr(running.as_ptr()) };
-        let mut writer_sneaker = ThreadSneaker::new(&mut queue);
         let writer_thread = std::thread::spawn(move || {
             let mut total_nanoseconds_writing = 0;
             let mut total_writes = 0;
-            let queue = writer_sneaker.get_item();
             while writer_running.load(Ordering::Relaxed) {
                 let start = std::time::Instant::now();
-                let _result = queue.try_write(112341234);
+                let _result = tx.send(112341234);
                 let duration = start.elapsed();
                 total_nanoseconds_writing += duration.as_nanos();
                 total_writes += 1;
